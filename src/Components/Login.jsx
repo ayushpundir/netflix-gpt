44fc7e60e6359714b8042c28ccfa70c94a35ac5e
Ayush_Pundir <ayushpundir11@gmail.com>
2026-01-06 16:39:50 +0530

    added signIn and signUp using firebase auth

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -54,7 +54,7 @@ const Login = () => {
 				// Signed in 
 				const user = userCredential.user;
 				// ...
-				console.log(user);
+				//console.log(user);
 			})
 			.catch((error) => {
 				const errorCode = error.code;
