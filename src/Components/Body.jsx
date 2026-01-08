7f135986a6c650fe2f8c3e08782fd5fa429d8d4c
Ayush_Pundir <ayushpundir11@gmail.com>
2026-01-08 21:39:07 +0530

    added onAuthStateChange api, updating store when user login or logout

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -2,8 +2,15 @@ import Login from "./Login";
 import Browse from "./Browse";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () =>{
+
+    const dispatch = useDispatch();
 
     const appRouter = createBrowserRouter([
             {
@@ -14,6 +21,25 @@ const Body = () =>{
                 element: <Browse/>,
             }
         ]);
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            console.log("user logged in");
+            // ...
+            dispatch(addUser({uid : uid, email: user.email, displayName: user.displayName, photoURL: user.photoURL}));
+            //navigate("/browse"); can't be done here
+            } else {
+                // User is signed out
+                // ...
+                dispatch(removeUser());
+                //navigate("/"); can't be done here 
+            }
+        });
+    }, []);
 
     return (
     <div>
