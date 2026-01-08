79b18f4d8dbd8b266a4e3d7caa158042823a741e
Ayush_Pundir <ayushpundir11@gmail.com>
2026-01-08 21:36:15 +0530

    updated header so it could be used on browse page and added signout api

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,38 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+
+    const navigate = useNavigate();
+    const user = useSelector((store) => store.user);
+
+    const handleSignOut = () => {
+        // Sign out logic here
+        signOut(auth).then(() => {
+        // Sign-out successful.
+        navigate("/");
+        
+        }).catch((error) => {
+        // An error happened.
+        });
+    }
+
+
     return (
-    <div className="absolute px-8 py-2 bg-linear-to-b from-black z-10">
+    <div className="absolute w-screen px-8 py-2 bg-linear-to-b from-black z-10 flex justify-between">
         <img className = "w-44"
         src="/Netflix_Logo_PMS.png"
         alt="LOGO" />
+
+        {user && (<div className="flex p-2">
+            <img
+            className = "w-12 h-12 "
+            src="/userIcon.png" 
+            //src = {user.photoURL}
+            alt="userIcon" />
+            <button onClick={handleSignOut} className="font-bold text-white">Sign out</button>
+        </div>)}
     </div>
     );
 };
