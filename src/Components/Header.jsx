71c1b5ce3ff581779a3c23c10f80b8f7e9ca4aa1
Ayush_Pundir <ayushpundir11@gmail.com>
2026-01-11 00:02:12 +0530

    fixed photo url issue every user was getting my picture, now fixed. on onAuthStateChange call in header. prevented direct access to main page and related issues

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,22 +2,46 @@ import { signOut } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 const Header = () => {
 
-    const navigate = useNavigate();
+
     const user = useSelector((store) => store.user);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleSignOut = () => {
         // Sign out logic here
         signOut(auth).then(() => {
-        // Sign-out successful.
-        navigate("/");
-        
+        // Sign-out successful.        
         }).catch((error) => {
         // An error happened.
         });
     }
 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/auth.user
+            const uid = user.uid;
+            // ...
+            dispatch(addUser({uid : uid, email: user.email, displayName: user.displayName, photoURL: user.photoURL}));
+            navigate("/browse");
+            } else {
+                // User is signed out
+                // ...
+                dispatch(removeUser());
+                navigate("/");
+            }
+        });
+
+        return () => unsubscribe(); // when the component unmounts this removes the listener
+    }, []);
 
     return (
     <div className="absolute w-screen px-8 py-2 bg-linear-to-b from-black z-10 flex justify-between">
@@ -28,8 +52,7 @@ const Header = () => {
         {user && (<div className="flex p-2">
             <img
             className = "w-12 h-12 "
-            src="/userIcon.png" 
-            //src = {user.photoURL}
+            src = {user.photoURL}
             alt="userIcon" />
             <button onClick={handleSignOut} className="font-bold text-white">Sign out</button>
         </div>)}
