62b0057a5bf23f27c5f02d1d4a101ad8cc682ef8
Ayush_Pundir <ayushpundir11@gmail.com>
2026-01-17 20:09:19 +0530

    made header responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -43,25 +43,27 @@ const Header = () => {
         return () => unsubscribe();
     }, []);
     return (
-        <div className="absolute w-full px-8 py-4 bg-linear-to-b from-black z-30 flex flex-col md:flex-row justify-between items-center">
-            <img className="w-44 mb-4 md:mb-0" src="/Netflix_Logo_PMS.png" alt="LOGO" />
+        <div className="absolute w-full px-4 md:px-8 py-2 md:py-4 bg-linear-to-b from-black z-30 flex justify-between items-center">
+            <img className="w-28 md:w-44" src="/Netflix_Logo_PMS.png" alt="LOGO" />
 
             {user && (
-                <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2 md:gap-4">
                     <button 
                         onClick={handleGptSearchButton}
-                        className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg hover:bg-purple-700 transition-all duration-200 font-medium shadow-md flex items-center gap-2"
+                        className="py-1.5 px-2 md:py-2 md:px-4 bg-purple-800 text-white text-xs md:text-base rounded-lg hover:bg-purple-700 transition-all duration-200 font-medium shadow-md flex items-center gap-1 md:gap-2"
                     >
-                        {showGptSearch ? "Homepage" : "✨ AI Search"}
+                        {showGptSearch ? "Home" : "✨ AI "}
+                        <span className="hidden md:inline">{showGptSearch ? "page" : " Search"}</span>
                     </button>
 
-                    <div className="flex items-center gap-4 bg-black/30 p-2 rounded-lg backdrop-blur-sm">
-                        <img className="w-10 h-10 rounded-md object-cover border border-gray-600" src={user.photoURL} alt="userIcon" />
+                    <div className="flex items-center gap-2 md:gap-4 bg-black/30 p-1.5 md:p-2 rounded-lg backdrop-blur-sm">
+                        <img className="w-8 h-8 md:w-10 md:h-10 rounded-md object-cover border border-gray-600" src={user.photoURL} alt="userIcon" />
                         <button 
                             onClick={handleSignOut} 
-                            className="font-semibold text-white bg-red-600 px-4 py-2 rounded-md hover:bg-red-700 transition-colors duration-200 shadow-lg"
+                            className="font-semibold text-white text-xs md:text-base bg-red-600 px-2 py-1.5 md:px-4 md:py-2 rounded-md hover:bg-red-700 transition-colors duration-200 shadow-lg"
                         >
-                            Sign Out
+                            <span className="md:hidden">Exit</span>
+                            <span className="hidden md:inline">Sign Out</span>
                         </button>
                     </div>
                 </div>
